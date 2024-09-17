c57a8aa098dc7a0f9ad17589413440621d74b34c
HaroldBarraza <132396790+HaroldBarraza@users.noreply.github.com>
2024-09-17 17:21:26 -0500

    sd

--- server.js
+++ server.js
@@ -6,9 +6,19 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+
+/* ***********************
+ * view engine and templates
+ *************************/
+app.set('view engine','ejs')
+app.use(expressLayouts);
+app.set('layout', './layouts/layout');
+
 
 /* ***********************
  * Routes
@@ -27,4 +37,9 @@ const host = process.env.HOST
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
+})
+
+/*index route*/
+app.get("/", function(req, res) {
+  res.render(index, {title:"Home"})
 })
