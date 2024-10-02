d031c20d87ed34090522c0bcba6e3f181bb4b090
HaroldBarraza <132396790+HaroldBarraza@users.noreply.github.com>
2024-10-02 14:38:48 -0500

    host

--- database/index.js
+++ database/index.js
@@ -28,6 +28,9 @@ module.exports = {
 else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl:{
+      rejectUnauthorized: false,
+    }
   })
   module.exports = pool;
 }
--- server.js
+++ server.js
@@ -62,11 +62,11 @@ app.use(async (err, req, res, next) => {
  * Values from .env (environment) file
  *************************/
 const port = process.env.PORT || 3000; 
-const host = process.env.HOST || 'localhost';
+const host = process.env.HOST || '0.0.0.0';
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`app listening on ${host}:${port}`);
 });
